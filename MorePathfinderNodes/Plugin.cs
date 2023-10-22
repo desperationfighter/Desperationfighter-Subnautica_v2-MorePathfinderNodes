55640f7a4d13d5bae44d414d658014ffb87ed1a8
Desperationfighter <patrick_as_desp@yahoo.de>
2023-10-22 17:32:44 +0200

    Add project files.

---
+++ MorePathfinderNodes/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+using MorePathfinderNodes.Managment;
+using Nautilus.Handlers;
+
+namespace MorePathfinderNodes
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("com.snmodding.nautilus")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public new static ManualLogSource Logger { get; private set; }
+
+        private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+        internal static IngameConfigMenu ICMConfig { get; private set; }
+
+        private void Awake()
+        {
+            // set project-scoped logger instance
+            Logger = base.Logger;
+
+            //Load Config and Ingame Menu
+            ICMConfig = OptionsPanelHandler.RegisterModOptions<IngameConfigMenu>();
+
+            // register harmony patches, if there are any
+            Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
